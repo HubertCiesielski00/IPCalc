7c4f9e9c9fcdd832b18b895456bd2b1be81f2b32
Hubert Ciesielski <hubert.ciesielski@onet.pl>
2020-10-11 20:37:16 +0200

    -Finished main tasks: -Validation script now checks if network mask is valid -Main script responnsible for calculating is done -Minor adjustments in colors -Added event for 'return' button

--- Script/Functions.js
+++ Script/Functions.js
@@ -1,18 +1,55 @@
 const Calculate = () =>{
+    try{
 
-    let Inputs = {
-        IP: document.querySelector("#IP"),
-        Mask : document.querySelector("#Mask"),
+        let Inputs = {
+            IP: document.querySelector("#IP"),
+            Mask : document.querySelector("#Mask"),
+            Network : document.querySelector("#NetWork"),
+            Broadcast : document.querySelector("#BroadCast")
+        }
+
+        let Ip = CutString(Inputs.IP.value);
+        let Mask = CutString(Inputs.Mask.value);
+        let Network = [];
+        let Broadcast = [];
+        let IpBinary = "";
+        let MaskBinary = "";
+        let NetworkBinary = "";
+        let BroadcastBinary = "";
+
+        for(let i = 0 ; i < 4; i++){
+            IpBinary += ConvertToBinary(Ip[i]);
+            MaskBinary += ConvertToBinary(Mask[i]);
+        }
+        if(IsMaskValid(MaskBinary)){
+            for(let i = 0; i < MaskBinary.length; i++){
+                    if (MaskBinary[i] == '1')
+                {
+                    NetworkBinary += IpBinary[i];
+                    BroadcastBinary += IpBinary[i];
+                }
+                if (MaskBinary[i] == "0"){
+                    NetworkBinary += '0';
+                    BroadcastBinary += '1';
+                }
+            }
+            
+        }
+        Network = CutAdress(NetworkBinary);
+        Broadcast = CutAdress(BroadcastBinary);
+        for(let i = 0; i < 4; i++){
+            Network[i] = ConvertToDecimal(Network[i]);
+            Broadcast[i] = ConvertToDecimal(Broadcast[i]);
+        }
+        Inputs.Network.value = `${Network[0]}.${Network[1]}.${Network[2]}.${Network[3]}`;
+        Inputs.Broadcast.value = `${Broadcast[0]}.${Broadcast[1]}.${Broadcast[2]}.${Broadcast[3]}`;
     }
-
-    let Ip = CutString(Inputs.IP.value);
-    let Mask = CutString(Inputs.Mask.value);
-
-
+    catch (error){
+        alert(error);
+    }
 }
 
 const CutString = Str =>{
-    try{
         if(!IpValidForm.test(Str)){
             throw "Wprowadzono niepoprawne dane";
         }
@@ -34,9 +71,68 @@ const CutString = Str =>{
             }
         }
         return Arr;
+
+}
+
+const ConvertToBinary = (Val) =>{
+    let result = "";
+   for(let i = 7; i >= 0; i--){
+       if(Val >= Power(2,i)){
+           result += "1";
+           Val -= Power(2,i);
+       }
+       else{
+           result += "0";
+       }
     }
-    catch (e){
-        alert(e);
+    return result;
+}
+
+const Power = (base, exponent) =>{
+    let result = base;
+    if(exponent == 0){
+        return 1;
     }
+    else{
+        for(let i = 1; i <exponent; i++)
+        {
+            result *= base;
+        }
+        return result;
+    }
+}
+const IsMaskValid = (Arr) =>{
+    
+    for(let i = 1; i < Arr.length; i++){
+        if (Arr[i] == '1' && Arr[i-1] == '0')
+        {
+            throw "Nieprawidłowa maska podsieci";
+            return false;
+            break;
+        }
+    }
+    
+    return true;
+}
+const CutAdress = (Adress) =>{
+    let temp = "";
+    const AdressToRet = [];
+    for(let i = 0; i < 4; i++){
+        for(let j = 0; j < 8; j++){
+            temp += Adress[8*i+j];
+        }
+        AdressToRet.push(temp);
+        temp = "";
+    }
+    return AdressToRet;
+}
+const ConvertToDecimal = (Arg) =>{
+    let temp = 0;
+    for(let i = Arg.length; i >=0 ; i--){
+        if(Arg[i] == "1"){
+            temp += Power(2, --Arg.length-i);
+        }
+    }
+    return temp;
 }
 
--- Script/index.js
+++ Script/index.js
@@ -3,4 +3,10 @@ const IpValidForm = /^[0-9]{1,3}[\.,][0-9]{1,3}[\.,][0-9]{1,3}[\.,][0-9]{1,3}$/
 window.onload = () =>{
     let Submit = document.querySelector("#Submit");
     Submit.addEventListener("click",Calculate);
+    addEventListener("keydown", (event) =>
+    {
+        if(event.key == "Enter"){
+            Calculate();
+        }
+    })
 }
